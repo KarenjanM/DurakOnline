48216062d21940f49834e293f4377bf14dfb9179
KarenjanTV <karenjantv@gmail.com>
2022-08-29 13:58:05 +0200

    finished main logic

---
+++ cards.py
@@ -0,0 +1,241 @@
+import random
+
+
+# suits
+SPADES = '♠'
+HEARTS = '♥'
+DIAMS = '♦'
+CLUBS = '♣'
+
+# ranks
+RANKS = ['6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
+
+NAME_TO_VALUE = {n: i for i, n in enumerate(RANKS)}
+
+CARDS_IN_HAND_MAX = 6
+
+N_PLAYERS = 2
+
+DECK = [(nom, suit) for nom in RANKS for suit in [SPADES, HEARTS, DIAMS, CLUBS]]
+
+
+class Player:
+    def __init__(self, index, cards):
+        self.index = index
+        self.cards = list(map(tuple, cards))
+
+    def take_cards_from_deck(self, deck: list):
+        """
+        Взять недостающее количество карт из колоды
+        Колода уменьшится
+        :param deck: список карт колоды
+        """
+        lack = max(0, CARDS_IN_HAND_MAX - len(self.cards))
+        n = min(len(deck), lack)
+        self.add_cards(deck[:n])
+        del deck[:n]
+        return self
+
+    def sort_hand(self):
+        """
+        Сортирует карты по достоинству и масти
+        """
+        self.cards.sort(key=lambda c: (NAME_TO_VALUE[c[0]], c[1]))
+        return self
+
+    def add_cards(self, cards):
+        self.cards += list(cards)
+        self.sort_hand()
+        return self
+
+    def take_card(self, card):
+        self.cards.remove(card)
+
+    @property
+    def n_cards(self):
+        return len(self.cards)
+
+    def __getitem__(self, item):
+        return self.cards[item]
+
+    def __repr__(self):
+        return f"Player{self.cards}"
+
+
+def rotate(l, n):
+    return l[n:] + l[:n]
+
+
+class Durak:
+    def __init__(self, chat_id, rng: random.Random = None):
+        self.rng = rng or random.Random()
+
+        self.deck = list(DECK)
+        self.rng.shuffle(self.deck)
+
+        self.players = [Player(i, []).take_cards_from_deck(self.deck)
+                        for i in range(N_PLAYERS)]
+
+        self.trump = self.deck[0][1]
+
+        # кладем козырь под низ вращая список по кругу на 1 назад
+        self.deck = rotate(self.deck, -1)
+
+        # игровое поле: ключ - атакующая карта, значения - защищающаяся или None
+        self.field = {}
+
+        self.attacker_chat_id = chat_id
+        self.attacker_index = 0
+        self.winner = None
+
+    @property
+    def attacking_cards(self):
+        """
+        List of attacking cards
+        """
+        return list(filter(bool, self.field.keys()))
+
+    @property
+    def defending_cards(self):
+        """
+        List of defending cards (filtering None)
+        """
+        return list(filter(bool, self.field.values()))
+
+    @property
+    def any_unbeaten_card(self):
+        """
+        If there is any unbeaten cards
+        """
+        return any(c is None for c in self.defending_cards)
+
+    @property
+    def current_player(self):
+        return self.players[self.attacker_index]
+
+    @property
+    def opponent_player(self):
+        return self.players[(self.attacker_index + 1) % N_PLAYERS]
+
+    def attack(self, card):
+        assert not self.winner  # игра не должна быть окончена!
+
+        if not self.can_add_to_field(card):
+            return False
+        cur, opp = self.current_player, self.opponent_player
+        cur.take_card(card)
+        self.field[card] = None
+        return True
+
+    def can_add_to_field(self, card):
+        if not self.field:
+            # it is always possible to add card to the field
+            return True
+
+        # среди всех атакующих и отбивающих карт ищем совпадения по достоинствам
+        for attack_card, defend_card in self.field.items():
+            if self.card_match(attack_card, card) or self.card_match(defend_card, card):
+                return True
+        return False
+
+    @staticmethod
+    def card_match(card1, card2):
+        if card1 is None or card2 is None:
+            return False
+        n1, _ = card1
+        n2, _ = card2
+        return n1 == n2
+
+    def defend(self, attacking_card, defending_card):
+        """
+        Защита
+        :param attacking_card: which card is to beat
+        :param defending_card: which card is to defend
+        :return: bool - success or not
+        """
+        assert not self.winner
+
+        if self.field[attacking_card] is not None:
+            # если эта карта уже отбита - уходим
+            return False
+        if self.can_beat(attacking_card, defending_card):
+            # еслии можем побить, то кладем ее на поле
+            self.field[attacking_card] = defending_card
+            # и изымаем из руки защищающегося
+            self.opponent_player.take_card(defending_card)
+            return True
+        return False
+
+    def can_beat(self, card1, card2):
+        """
+        Does card1 beat card2?
+        """
+        nom1, suit1 = card1
+        nom2, suit2 = card2
+
+        nom1 = NAME_TO_VALUE[nom1]
+        nom2 = NAME_TO_VALUE[nom2]
+
+        if suit2 == self.trump:
+            # if trump, then beat any card, which is not trump or trump but with lower rank
+            return suit1 != self.trump or nom2 > nom1
+        elif suit1 == suit2:
+            # otherwise suits should be equal and lower rank
+            return nom2 > nom1
+        else:
+            return False
+
+    NORMAL = 'normal'
+    TOOK_CARDS = 'took_cards'
+    GAME_OVER = 'game_over'
+
+    def is_no_cards(self):
+        if not self.deck:
+            if not self.current_player.cards:
+                return True
+        if not self.opponent_player.cards:
+            return True
+        return False
+
+    @property
+    def attack_succeed(self):
+        return any(def_card is None for def_card in self.field.values())
+
+    def finish_turn(self):
+        assert not self.winner
+
+        took_cards = False
+        if self.attack_succeed:
+            print('there is some unbeaten card')
+            # take all cards from the field if player couldn't defend
+            self._take_all_field()
+            took_cards = True
+        else:
+            # all cards are beaten, so just clearing the field
+            self.field = {}
+
+        # очередность взятия карт из колоды определяется индексом атакующего (можно сдвигать на 1, или нет)
+        for p in rotate(self.players, self.attacker_index):
+            p.take_cards_from_deck(self.deck)
+
+        # is deck empty?
+        if not self.deck:
+            for p in self.players:
+                if not p.cards:
+                    self.winner = p.index
+                    return self.GAME_OVER
+
+        if took_cards:
+            return self.TOOK_CARDS
+        else:
+            # turn is changed, if player has successfully defended
+            self.attacker_index = self.opponent_player.index
+            return self.NORMAL
+
+    def _take_all_field(self):
+        """
+        Opponent takes all the cards from the field
+        """
+        cards = self.attacking_cards + self.defending_cards
+        self.opponent_player.add_cards(cards)
+        self.field = {}
--- main.py
+++ main.py
@@ -1,12 +1,10 @@
 import logging
-
-from sqlalchemy.exc import ProgrammingError
-from sqlalchemy.sql import exists
-
 from aiogram import Bot, Dispatcher, executor, types
-from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton, \
+    ReplyKeyboardMarkup, ReplyKeyboardRemove, KeyboardButton
 from aiogram.utils.callback_data import CallbackData
 from sqlalchemy import create_engine, Table, Column, Integer, String, MetaData
+from cards import Durak, DECK
 
 meta = MetaData()
 
@@ -49,7 +47,7 @@ async def get_users(message: types.Message):
     for row in result:
         if message.chat.username == row[1]:
             continue
-        s = users.select().where(username=row[1])
+        s = users.select().where(users.c.username == row[1])
         chat_id = conn.execute(s).first()[2]
         keyboard.add(InlineKeyboardButton(text=row[1], callback_data=connect_cb.new(action='request', chat_id=chat_id,
                                                                                     username=row[1])))
@@ -66,11 +64,32 @@ def exists_in_table(conn, table, chat_id):
         return True
 
 
+def get_partner(message):
+    q = partners.select().where(partners.c.chat_id == message.chat.id)
+    result = conn.execute(q)
+    return result
+
+
 # creating a pull of players to choose
 @dp.message_handler(commands=['play'])
 async def play(message: types.Message):
     await message.answer("That's great, now choose user, which with you'd like to play:")
     await get_users(message)
+
+messages = {}
+
+
+# async def create_or_edit_field(durak, chat_id=None, message: types.Message = None):
+#     text = ''
+#     for attacking_cards, defending_cards in durak.field.items():
+#         if defending_cards is None:
+#             text += f'{attacking_cards}'
+#         text += f'{attacking_cards}/{defending_cards}\t'
+#     try:
+#         await message.edit_text(text)
+#     except Exception:
+#         msg = await bot.send_message(text, chat_id)
+#         messages[chat_id] = msg
 
 
 # deleting a session between users
@@ -81,48 +100,91 @@ async def quit_dialog(message: types.Message):
             q = partners.select().where(partners.c.chat_id == message.chat.id)
             result = conn.execute(q)
             row = result.first()
-            await bot.send_message(row[2],
-                                   'The connection was cut off by other user. Hope you had a good time')
+            print(row)
+            durak_games.pop(str(row[1]), None)
+            durak_games.pop(str(message.chat.id), None)
+            await bot.send_message(row[1],
+                                   'The connection was cut off by other user. Hope you had a good time',
+                                   reply_markup=ReplyKeyboardRemove())
         else:
-            raise ProgrammingError
-        await message.answer('Hope you had good time')
+            raise Exception
+        await message.answer('Hope you had good time', reply_markup=ReplyKeyboardRemove())
         delete_q1 = partners.delete().where(partners.c.chat_id == message.chat.id)
-        delete_q2 = partners.delete().where(partners.c.chat_id == row[2])
+        delete_q2 = partners.delete().where(partners.c.chat_id == row[1])
         conn.execute(delete_q1)
         conn.execute(delete_q2)
-    except ProgrammingError:
+    except Exception:
         await message.answer("You don't have any partners yet. Send me /play and I'll find you somebody to play with:)")
 
 
 # requesting chosen user to create a connection
 @dp.callback_query_handler(connect_cb.filter(action='request'))
 async def request_to_connect(callback_query: types.CallbackQuery, callback_data: dict):
-    await callback_query.answer('Request is sent. Waiting for response...')
-    keyboard = InlineKeyboardMarkup(row_width=2)
-    keyboard.add(InlineKeyboardButton('Yes', callback_data=connect_cb.new(action='accepted',
-                                                                          chat_id=callback_query.message.chat.id,
-                                                                          username=callback_query.message.chat.username)))
-    keyboard.add(InlineKeyboardButton('No', callback_data=connect_cb.new(action='rejected',
-                                                                         chat_id=callback_query.message.chat.id,
-                                                                         username=callback_query.message.chat.username)))
-    await bot.send_message(callback_data['chat_id'],
-                           f"There is request from {callback_data['username']}. Do you want to play with him/her? ",
-                           reply_markup=keyboard)
+    if exists_in_table(conn, partners, callback_query.message.chat.id):
+        await callback_query.answer('You already have one connection and you cannot create a new one. '
+                                    'Send me /quit if you want to terminate this one.')
+    else:
+        await callback_query.answer('Request is sent. Waiting for response...')
+        keyboard = InlineKeyboardMarkup(row_width=2)
+        keyboard.add(InlineKeyboardButton('Yes', callback_data=connect_cb.new(action='accepted',
+                                                                              chat_id=callback_query.message.chat.id,
+                                                                              username=callback_query.message.chat.username)))
+        keyboard.add(InlineKeyboardButton('No', callback_data=connect_cb.new(action='rejected',
+                                                                             chat_id=callback_query.message.chat.id,
+                                                                             username=callback_query.message.chat.username)))
+        await bot.send_message(callback_data['chat_id'],
+                               f"There is request from {callback_query.message.chat.username}. "
+                               f"Do you want to play with him/her? ",
+                               reply_markup=keyboard)
+
+
+def get_card_hand_kb(cards_list, player_index, turn):
+    buttons = [[]]
+    list_index = 0
+    counter = 0
+    for rank, suit in cards_list:
+        buttons[list_index].append(KeyboardButton(f'{rank} {suit}'))
+        counter += 1
+        if counter % 6 == 0:
+            buttons.append([])
+            list_index += 1
+    if player_index == turn:
+        buttons.append([KeyboardButton('Отбой')])
+    else:
+        buttons.append([KeyboardButton('Забрать карты')])
+    kb = ReplyKeyboardMarkup(keyboard=buttons, resize_keyboard=True)
+
+    return kb
+
+
+durak_games = dict()
 
 
 # accepting request and establishing connection. Adding users to database 'partners'
 @dp.callback_query_handler(connect_cb.filter(action='accepted'))
 async def request_accepted(callback_query: types.CallbackQuery, callback_data: dict):
-    if not exists_in_table(conn, partners, callback_query.message.chat.id):
+    if exists_in_table(conn, partners, callback_query.message.chat.id):
+        await callback_query.answer('You already have one connection and you cannot create a new one. '
+                                    'Send me /quit if you want to terminate this one.')
+    else:
         ins_1 = partners.insert().values(chat_id=callback_query.message.chat.id,
                                          partner_chat_id=callback_data['chat_id'])
         ins_2 = partners.insert().values(chat_id=callback_data['chat_id'],
                                          partner_chat_id=callback_query.message.chat.id)
         conn.execute(ins_1)
         conn.execute(ins_2)
-    await callback_query.message.answer('Connection is established. Now you can send any message to your partner!')
-    await bot.send_message(callback_data['chat_id'],
-                           'Your request was accepted. Now you can send any message to your partner!')
+        durak = durak_games[str(callback_query.message.chat.id)] = Durak(callback_query.message.chat.id)
+        durak_games[str(callback_data['chat_id'])] = durak
+        durak.current_player.chat_id = callback_query.message.chat.id
+        durak.opponent_player.chat_id = callback_data['chat_id']
+        kb_1 = get_card_hand_kb(durak.current_player.cards, durak.current_player.index, durak.attacker_index)
+        kb_2 = get_card_hand_kb(durak.opponent_player.cards, durak.opponent_player.index, durak.attacker_index)
+        await callback_query.message.answer('The game is started!GL HF', reply_markup=kb_1)
+        await callback_query.message.answer(f'The trump is {durak.trump}.')
+        await bot.send_message(callback_data['chat_id'],
+                               'Your request was accepted. The game is started! GL HF', reply_markup=kb_2)
+        await bot.send_message(callback_data['chat_id'],
+                               f'The trump is {durak.trump}.')
 
 
 # rejecting a request
@@ -134,15 +196,103 @@ async def request_rejected(callback_query: types.CallbackQuery, callback_data: d
     await get_users(callback_query.message)
 
 
+@dp.message_handler(lambda msg: msg.text == 'Отбой')
+async def attacker_finish(message: types.Message):
+    result = get_partner(message)
+    partner_chat_id = result.fetchone()[1]
+    durak = durak_games[str(message.chat.id)]
+    response = durak.finish_turn()
+    if durak.attack_succeed and response != 'game_over':
+        await message.answer('Not all your cards is beaten yet')
+    else:
+        if response == 'normal':
+            durak.attacker_chat_id = partner_chat_id
+            await message.answer('The round is over')
+            await message.answer('The opponent player is now going to play. You defend.',
+                                 reply_markup=get_card_hand_kb(durak.opponent_player.cards,
+                                                               durak.opponent_player.index,
+                                                               durak.attacker_index))
+            await bot.send_message(text='The round is over', chat_id=partner_chat_id)
+            await bot.send_message(text='You have beaten all the cards. It is your turn to attack!',
+                                   chat_id=partner_chat_id,
+                                   reply_markup=get_card_hand_kb(durak.current_player.cards,
+                                                                 durak.current_player.index,
+                                                                 durak.attacker_index))
+        elif response == 'game_over':
+            await message.answer('You have won!!! Congrats!', reply_markup=ReplyKeyboardRemove())
+            await bot.send_message(partner_chat_id, 'You lose. You are DURAK.', reply_markup=ReplyKeyboardRemove())
+            await quit_dialog(message)
+
+
+@dp.message_handler(lambda msg: msg.text == 'Забрать карты')
+async def defender_finish(message: types.Message):
+    result = get_partner(message)
+    partner_chat_id = result.fetchone()[1]
+    durak = durak_games[str(message.chat.id)]
+    if not durak.attack_succeed:
+        await message.answer('You have beaten all the cards for now. You do not need to take any card')
+    else:
+        print(durak.field)
+        response = durak.finish_turn()
+        print(response, 'response')
+        if response == 'took_cards':
+            await message.answer('You have just taken all the cards from the field. You are going to defend again.',
+                                 reply_markup=get_card_hand_kb(durak.opponent_player.cards,
+                                                               durak.opponent_player.index,
+                                                               durak.attacker_index)
+                                 )
+            await bot.send_message(partner_chat_id, 'Opponent player took all of the cards. You can attack again!',
+                                   reply_markup=get_card_hand_kb(durak.current_player.cards,
+                                                                 durak.current_player.index,
+                                                                 durak.attacker_index))
+        elif response == 'game_over':
+            await message.answer('You have won!!! Congrats!', reply_markup=ReplyKeyboardRemove())
+            await bot.send_message(partner_chat_id, 'You lose. You are DURAK.', reply_markup=ReplyKeyboardRemove())
+            await quit_dialog(message)
+
+
+@dp.message_handler(lambda msg: (msg.text.split()[0], msg.text.split()[1]) in DECK)
+async def round_play(message: types.Message):
+    result = get_partner(message)
+    partner_chat_id = result.fetchone()[1]
+    durak = durak_games[str(message.chat.id)]
+    print(durak, 'object')
+    if durak.attacker_chat_id == message.chat.id:
+        if durak.attack((message.text.split()[0], message.text.split()[1])):
+            await message.answer('Accepted!', reply_markup=get_card_hand_kb(durak.current_player.cards,
+                                                                            durak.current_player.index,
+                                                                            durak.attacker_index))
+            if durak.is_no_cards():
+                await attacker_finish(message)
+            await bot.send_message(partner_chat_id, message.text)
+        else:
+            await message.answer('You cannot play this card right now!')
+    else:
+        is_defended = False
+        for card in durak.attacking_cards:
+            if durak.defend(card, (message.text.split()[0], message.text.split()[1])):
+                await message.answer('Accepted!', reply_markup=get_card_hand_kb(durak.opponent_player.cards,
+                                                                                durak.opponent_player.index,
+                                                                                durak.attacker_index))
+                is_defended = True
+                if durak.is_no_cards():
+                    await defender_finish(message)
+                await bot.send_message(partner_chat_id, message.text)
+                break
+        if not is_defended:
+            await message.answer('You cannot play this card right now!')
+        # await create_or_edit_field(messages[message.chat.id])
+    # except Exception:
+    # await message.answer("You don't have any partners yet. Send me /play and I'll find you somebody to play with:)")
+
+
 @dp.message_handler()
-async def dialogue(message: types.Message):
-    q = partners.select().where(partners.c.chat_id == message.chat.id)
-    result = conn.execute(q)
+async def dialog(message: types.Message):
     try:
+        result = get_partner(message)
         partner_chat_id = result.fetchone()[1]
         await bot.send_message(partner_chat_id, message.text)
     except Exception:
         await message.answer("You don't have any partners yet. Send me /play and I'll find you somebody to play with:)")
-
 
 executor.start_polling(dispatcher=dp, skip_updates=True)
